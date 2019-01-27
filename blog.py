543a40ae7ee8c836a02336c13ef2b36615a95a94
Jonny Perrin <jonathan.w.perrin@gmail.com>
2019-01-27 15:07:02 -0500

    added logic to display posts

--- blog.py
+++ blog.py
@@ -4,6 +4,7 @@
 from flask import Flask, render_template, request, session, \
     flash, redirect, url_for, g
 import sqlite3
+from functools import wraps
 
 
 #configuration
@@ -21,7 +22,17 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to log in first.')
+            return redirect(url_for('login'))
+    return wrap
+
+@app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
     status_code = 200
@@ -37,8 +48,13 @@ def login():
     
 
 @app.route('/main')
+@login_required
 def main():
-    return render_template('main.html')
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts=posts)
 
 @app.route('/logout')
 def logout():
